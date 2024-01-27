1705b51adc5ea7ca12668f3dc6ed48ee95f2ca5f
EC2 Default User <ec2-user@ip-172-31-33-157.ec2.internal>
2024-01-27 17:07:55 +0000

    initial commit

---
+++ Lob2/src/scripts/sublob1/mystr_01.py
@@ -0,0 +1,8 @@
+class MyString1:
+    def __init__(self, s):
+        self.s = s
+
+    def check_len(self):
+        print('check_len')
+        return len(self.s)
+
---
+++ Lob2/src/scripts/sublob3/mystr_03.py
@@ -0,0 +1,8 @@
+class MyString3:
+    def __init__(self, s):
+        self.s = s
+
+    def case_change(self):
+        print('case_change')
+        return self.s.upper()
+
